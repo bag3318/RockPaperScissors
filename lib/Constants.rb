ee6a95b630daebb5a31c8b8e5564ba3f102f9529
bag3318 <bglatman@outlook.com>
2017-04-05 07:52:44 -0700

    fix

--- lib/Constants.rb
+++ lib/Constants.rb
@@ -7,13 +7,14 @@ module Constants
     'r' => :ROCK, 
     's' => :SCISSORS 
   } 
-  VALID_ENTRIES = NTRY_TO_SYM.keys 
-  COMPUTER_CHOICES = NTRY_TO_SYM.values
+  VALID_ENTRIES = NTRY_TO_SYM.keys # create valid entries
+  COMPUTER_CHOICES = NTRY_TO_SYM.values # define computer choices
   WINNERS = [
+    # format: player choice, computer choice
     [:SCISSORS, :PAPER], 
     [:PAPER, :ROCK], 
     [:ROCK, :SCISSORS]
-  ] # format: player choice, computer choice
+  ] 
   LOSERS = WINNERS.map { |i,j| [j,i] } # this will take the original WINNERS array and flip the symbols, thus returning a loss for the user/player
   INIT_STRINGS = [
     ColorizedString["You are about to enter a rock-paper-scissors best of 3 match."].colorize(:green), 
